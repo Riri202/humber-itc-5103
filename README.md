98da522801e1ef5c4220bc19d292c84dc0ac63e1
Rita-Maria <ritamariaoladokun@gmail.com>
2024-03-06 08:27:56 -0500

    create repo and edit read-me file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # humber-itc-5103
+This is a repo of all the class activities, exercises, labs and projects for my Humber web dev class.
---
+++ week6/js/script.js
@@ -0,0 +1,3 @@
+const miles = prompt("Enter miles");
+const gallons = prompt("Enter gallons");
+alert(`You will use ${miles} miles per ${gallons} gallons`);
